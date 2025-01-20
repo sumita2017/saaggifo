82f9a216651925eb57bebaecbb082983a359be92
sumita2017 <dassumita91@gmail.com>
2025-01-20 10:49:18 +0530

    git acces

--- resources/views/front/career.blade.php
+++ resources/views/front/career.blade.php
@@ -11,6 +11,7 @@
         <h4 class="text-white display-4 mb-4 wow fadeInDown" data-wow-delay="0.1s">Career</h4>
         <ol class="breadcrumb justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
             <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <!-- <li class="breadcrumb-item active text-primary">Career</li> -->
             <li class="breadcrumb-item active text-primary">Career</li>
         </ol>
     </div>
